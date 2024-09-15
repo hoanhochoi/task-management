12be777f89ac78b336ba3699ba13c32eae8d1f32
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-09-15 10:39:17 +0700

    cài đặt cors

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express")
 require("dotenv").config();
 const database = require("./config/database")
 const bodyParser = require('body-parser')
+var cors = require('cors')
 const routesApiVer1 = require("./api/v1/routes/index.route")
 
 database.connect();
 const app = express()
 const port = process.env.PORT;
+
+// cors
+app.use(cors()) // cross-origin resource
+// end cors
 
 // parse application/json
 app.use(bodyParser.json())
